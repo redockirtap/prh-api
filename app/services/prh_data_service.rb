f345ea5fb8cb2acce5622c3dbe22bbf2dcead832
redockirtap <redockirtap@gmail.com>
2023-03-27 23:22:26 +0300

    refactor code in service

--- app/services/prh_data_service.rb
+++ app/services/prh_data_service.rb
@@ -3,9 +3,7 @@ require 'rest-client'
 class PrhDataService
   def fetch_data
     postal_codes.each_with_object([]) do |postal_code, companies|
-      url_params = { streetAddressPostCode: postal_code, maxResults: ENV['MAX_RESULTS'] }
-      response = RestClient.get(ENV['BASE_URL'], params: url_params)
-      postal_code_companies = JSON.parse(response.body)['results']
+      postal_code_companies = fetch_companies(postal_code)
       postal_code_companies.each { |company| companies << company }
       save_postal_codes(postal_code)
       save_companies(companies, postal_code)
@@ -14,23 +12,37 @@ class PrhDataService
 
   private
 
+  def fetch_companies(postal_code)
+    url_params = { streetAddressPostCode: postal_code, maxResults: ENV['MAX_RESULTS'] }
+    res = RestClient.get(ENV['BASE_URL'], params: url_params)
+    JSON.parse(res.body)['results']
+  end
+
   def save_postal_codes(postal_code)
+    return if PostalCode.find_by(postal_code: postal_code)
+
     @postal_code = PostalCode.new(postal_code: postal_code)
     @postal_code.update(postal_code: postal_code)
   end
 
   def save_companies(companies, postal_code)
     companies.each do |company|
-      @postal_code = PostalCode.find_by(postal_code: postal_code)
-      @company = Company.new(business_id: company['businessId'], postal_code_id: @postal_code.id)
+      next if Company.find_by(business_id: company['businessId'])
 
-      @company.update(name: company['name'],
-                      business_id: company['businessId'],
-                      registration_date: company['registrationDate'],
-                      company_form: company['companyForm'],
-                      details_uri: company['detailsUri'],
-                      postal_code_id: @postal_code.id)
+      update_company_record(company, postal_code)
     end
+  end
+
+  def update_company_record(company, postal_code)
+    @postal_code = PostalCode.find_by(postal_code: postal_code)
+    @company = Company.new(business_id: company['businessId'], postal_code_id: @postal_code.id)
+
+    @company.update(name: company['name'],
+                    business_id: company['businessId'],
+                    registration_date: company['registrationDate'],
+                    company_form: company['companyForm'],
+                    details_uri: company['detailsUri'],
+                    postal_code_id: @postal_code.id)
   end
 
   def postal_codes
